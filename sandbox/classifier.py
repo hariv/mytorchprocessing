e1c9568cba53dde5286b07fb1426e097544ef1aa
hariv <vngpln@yahoo.co.in>
2018-07-04 01:00:22 -0700

    Added training code

--- sandbox/classifier.py
+++ sandbox/classifier.py
@@ -1,13 +1,38 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
+import cv2
+import torch.optim as optim
 
 def imshow(img):
 	img = img/2 + 0.5
 	npimg = img.numpy()
-	plt.imshow(np.transpose(npimg, (1, 2, 0)))
+	#print(npimg.shape)
+	cv2.imshow("image", np.transpose(npimg, (1,2,0)))
+	#cv2.waitKey()
+	#plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(3, 6, 5)
+		self.pool = nn.MaxPool2d(2, 2)
+		self.conv2 = nn.Conv2d(6, 16, 5)
+		self.fc1 = nn.Linear(16 * 5 * 5, 120)
+		self.fc2 = nn.Linear(120, 84)
+		self.fc3 = nn.Linear(84, 10)
+
+	def forward(self, x):
+		x = self.pool(F.relu(self.conv1(x)))
+		x = self.pool(F.relu(self.conv2(x)))
+		x = x.view(-1, 16 * 5 * 5)
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+		return x
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -30,3 +55,26 @@ images, labels = dataiter.next()
 
 imshow(torchvision.utils.make_grid(images))
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):
+	running_loss = 0.0
+	for i, data in enumerate(trainloader, 0):
+		inputs, labels = data
+		optimizer.zero_grad()
+		outputs = net(inputs)
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+		running_loss += loss.item()
+		if i % 2000 == 1999:
+			print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+			running_loss = 0.0
+
+print("Done training")
+
