6d4c510e8dfd3b527f741ce48d7f3e3bf4dd3d86
hariv <vngpln@yahoo.co.in>
2018-07-06 02:02:52 -0700

    Added image preprocessing to pass to mdoel

--- src/classifier.py
+++ src/classifier.py
@@ -1,12 +1,34 @@
 from __future__ import print_function, division
 import torch
 import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
 import time
 import os
 import copy
+import json
+from torch.autograd import Variable
+from PIL import Image
+import numpy as np
 
-class Classifier(nn.Module):
+class Classifier():
 	
 	def __init__(self, category, model="alexnet"):
 		self.model = torchvision.models.__dict__[model](pretrained=True)
 		self.category = category
+
+	def predict(self, img):
+		prediction = self.model(img)
+		return prediction
+
+classifier = Classifier(category="dog")
+#imsize = 256*6*6
+#loader = transforms.Compose([transforms.Resize(imsize), transforms.ToTensor()])
+
+im = np.array(Image.open('/Users/harivenugopalan/Downloads/1.jpg').convert("RGB"))
+im = Variable(torch.Tensor(im))
+im = im.unsqueeze(0)
+#im = loader(im).float()
+#im = Variable(im, requires_grad=True)
+print("hi")
+print(classifier.predict(im))
