72718c5c5c02c0001a0d5e55cf19fcdad0005b18
hariv <vngpln@yahoo.co.in>
2018-07-07 23:26:25 -0700

    Showcased more features in demo.py

--- src/demo.py
+++ src/demo.py
@@ -3,6 +3,11 @@ from labelParser import LabelParser
 classifier = Classifier(categories=['street sign,', 'suit,', 'magpie,', 'jay,', 'sunglasses,'])
 image = classifier.readImage('../1.jpg')
 image = classifier.prepare(image)
-probability = classifier.getPredictionProbability(image)
 
+probability = classifier.getPredictionProbability(image)
 print(probability)
+
+print(classifier.getTopClassLabel(probability))
+print(classifier.getTopKProbabilities(probability, 5))
+print(classifier.getTopKClassLabels(probability, 5))
+print(classifier.getCategoryProbabilities(probability))
