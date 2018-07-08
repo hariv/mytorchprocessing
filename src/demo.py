74dc13ba2976448c32ff3fe124aaff2210d5a673
hariv <vngpln@yahoo.co.in>
2018-07-07 23:22:56 -0700

    Added demo sample image

--- src/demo.py
+++ src/demo.py
@@ -1,7 +1,7 @@
 from labelParser import LabelParser
 
 classifier = Classifier(categories=['street sign,', 'suit,', 'magpie,', 'jay,', 'sunglasses,'])
-image = classifier.readImage('./1.jpg')
+image = classifier.readImage('../1.jpg')
 image = classifier.prepare(image)
 probability = classifier.getPredictionProbability(image)
 
