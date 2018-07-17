6f81a9419a4d25943f552e5801fcf6865448b36c
hariv <vngpln@yahoo.co.in>
2018-07-16 23:44:24 -0700

    Added test to check probability from energy

--- tests/classifierTest.py
+++ tests/classifierTest.py
@@ -1,6 +1,9 @@
 import unittest
 
 class ClassifierTest(unittest.TestCase):
-
+	
+	def testGetProbability(self):
+		classifier = Classifier()
+		self.assertEqual(classifier.getProbability(1), nn.functional.softmax(1,dim=1))
 if __name__ == '__main__':
     unittest.main()
