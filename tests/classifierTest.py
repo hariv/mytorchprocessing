ed599d6929aee644de2e232f287fb38dcc367cc8
hariv <vngpln@yahoo.co.in>
2018-07-17 16:07:51 -0700

    Converted to package

--- tests/classifierTest.py
+++ tests/classifierTest.py
@@ -1,10 +1,10 @@
 import unittest
 import torch
 import torch.nn as nn
-from classifier import Classifier
+import sys
+from ..src.classifier import Classifier
 
-class ClassifierTest(unittest.TestCase):
-	
+class ClassifierTest(unittest.TestCase):	
 	def testGetProbability(self):
 		classifier = Classifier()
 		self.assertEqual(classifier.getProbability(1), nn.functional.softmax(1,dim=1))
