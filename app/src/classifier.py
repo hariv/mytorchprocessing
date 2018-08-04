5f761ed9b84a36e3389c97071e80fd21db0260dd
hariv <vngpln@yahoo.co.in>
2018-08-04 16:49:55 -0700

    Added train code

--- app/src/classifier.py
+++ app/src/classifier.py
@@ -49,7 +49,7 @@ class Classifier():
 		img = img / 2 + 0.5
 		npimg = img.numpy()
 		plt.imshow(np.transpose(npimg, (1, 2, 0)))
-		
+
 	def prepare(self, img):
 		img = Classifier.preprocess(img)
 		img.unsqueeze_(0)
@@ -99,3 +99,27 @@ class Classifier():
 		indices = np.where(np.isin(self.labels, self.categories))[0]
 		probabilities = probability.detach().numpy()[0]
 		return probabilities[indices]
+
+	def train:
+		for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
