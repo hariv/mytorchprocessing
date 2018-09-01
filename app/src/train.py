9b4d51f3546f36913243eada00078218968d765e
hariv <vngpln@yahoo.co.in>
2018-09-01 01:57:48 -0700

    Added base model implementation

--- app/src/train.py
+++ app/src/train.py
@@ -1,5 +1,14 @@
 from options.train_options import TrainOptions
+from data import CreateDataLoader
+from models import create_model
+
 
 if __name__ == '__main__':
 	opt = TrainOptions().parse()
-	#data_loader = CreateDataLoader(opt)
+	data_loader = CreateDataLoader(opt)
+	dataset = data_loader.load_data()
+
+	dataset_size = len(data_loader)
+    print('#training images = %d' % dataset_size)
+
+    
