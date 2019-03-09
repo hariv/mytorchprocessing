d53a91c51f41b9ded7c5e4aca38a21df737ff775
hariv <vngpln@yahoo.co.in>
2019-03-08 17:50:43 -0800

    Changed default setting of model to test

--- classifier/options/test_options.py
+++ classifier/options/test_options.py
@@ -6,7 +6,6 @@ class TestOptions(BaseOptions):
         parser = BaseOptions.initialize(self, parser)
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-        parser.set_defaults(model='test')
         self.isTrain = False
         
         return parser
