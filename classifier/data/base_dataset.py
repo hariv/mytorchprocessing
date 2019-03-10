96a2801ac77eec362fb0cc12f94131f2190b475d
hariv <vngpln@yahoo.co.in>
2019-03-09 22:18:57 -0800

    Added generic handling of preprocessing for loading pretrained code

--- classifier/data/base_dataset.py
+++ classifier/data/base_dataset.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import torch.utils.data as data
 from PIL import Image
-import torchvision
+import torchvision.transforms as transforms
 import os
 
 class BaseDataset(data.Dataset):
@@ -10,15 +10,17 @@ class BaseDataset(data.Dataset):
     def __init__(self, opt):
         self.root = opt.dataroot
         self.normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-
+        
+        self.preprocess = None
+        if(opt.pretrained):
+            self.preprocess = transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize])
+        
         if(opt.isTrain):
-            self.dataset = torchvision.datasets.ImageFolder(os.path.join(self.root, 'train'), torchvision.transforms.Compose([
-                torchvision.transforms.RandomResizedCrop(224),
-                torchvision.transforms.RandomHorizontalFlip(),
-                torchvision.transforms.ToTensor(), self.normalize]))
+            self.dataset = torchvision.datasets.ImageFolder(os.path.join(self.root, 'train'), self.preprocess)
         else:
-            self.dataset = torchvision.datasets.ImageFolder(os.path.join(self.root, 'test'), torchvision.transforms.Compose([
-                torchvision.transforms.Resize(256),
-                torchvision.transforms.CenterCrop(224),
-                torchvision.transforms.ToTensor(), self.normalize]))    
+            self.dataset = torchvision.datasets.ImageFolder(os.path.join(self.root, 'test'), self.preprocess)    
         
