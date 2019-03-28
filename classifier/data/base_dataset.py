3f99e2230715ca9603678a69a645ee016b346263
hariv <vngpln@yahoo.co.in>
2019-03-28 16:43:48 -0700

    Added code to get generic mean and std for dataset

--- classifier/data/base_dataset.py
+++ classifier/data/base_dataset.py
@@ -10,7 +10,21 @@ class BaseDataset(data.Dataset):
 
     def __init__(self, opt):
         self.root = opt.dataroot
-        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        
+        self.preprocess = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor()
+                ])
+        
+        if(opt.isTrain):
+            self.dataset = datasets.ImageFolder(os.path.join(self.root, 'train'), self.preprocess)
+        else:
+            self.dataset = datasets.ImageFolder(os.path.join(self.root, 'test'), self.preprocess)
+        
+        self.compute_statistics()
+        
+        self.normalize = transforms.Normalize(mean=self.mean, std=self.std)
         
         self.preprocess = transforms.Compose([
                 transforms.Resize(256),
@@ -21,5 +35,21 @@ class BaseDataset(data.Dataset):
         if(opt.isTrain):
             self.dataset = datasets.ImageFolder(os.path.join(self.root, 'train'), self.preprocess)
         else:
-            self.dataset = datasets.ImageFolder(os.path.join(self.root, 'test'), self.preprocess)    
+            self.dataset = datasets.ImageFolder(os.path.join(self.root, 'test'), self.preprocess)
+        
+    def compute_statistics(self):
+        sumEl = 0.0
+        countEl = 0
+        
+        for img, _ in self.dataset:
+            sumel += img.sum([1, 2])
+            countel += torch.numel(img[0])
+        
+        self.mean = sumel/countel
+            
+        for img, _ in self.dataset:
+            img = (img - mean.unsqueeze(1).unsqueeze(1))**2
+            sumel += img.sum([1, 2])
+            countel += torch.numel(img[0])
+        self.std = torch.sqrt(sumel/countel)
         
