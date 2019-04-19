57898a46fd8d3b4a5b1224c718478b4ad9dbe6bb
hariv <vngpln@yahoo.co.in>
2019-04-19 15:35:20 -0700

    Resolved pretraining bug

--- classifier/data/base_dataset.py
+++ classifier/data/base_dataset.py
@@ -11,28 +11,28 @@ class BaseDataset(data.Dataset):
 
     def __init__(self, opt):
         self.root = opt.dataroot
-        
+
         self.preprocess = transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor()
-                ])
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor()])
         
         if(opt.isTrain):
             self.dataset = datasets.ImageFolder(os.path.join(self.root, 'train'), self.preprocess)
         else:
             self.dataset = datasets.ImageFolder(os.path.join(self.root, 'test'), self.preprocess)
+
         
         self.compute_statistics()
         
         self.normalize = transforms.Normalize(mean=self.mean, std=self.std)
-        
+
         self.preprocess = transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                self.normalize])
-        
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            self.normalize])
+            
         if(opt.isTrain):
             self.dataset = datasets.ImageFolder(os.path.join(self.root, 'train'), self.preprocess)
         else:
